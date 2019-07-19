781ef28bd6c00c6ebfd8bbd881231895aab8ab47
Crystal <czhu2020@spyponders.com>
2019-07-19 11:13:02 -0400

    yeh

--- read_database_human.py
+++ read_database_human.py
@@ -5,7 +5,7 @@ from camera import take_picture #removing this makes it die for dumb reasons
 
 from descriptors import add_to_database
 
-filename = input("Filename?\n")
+filename = input("Database pathname?\n")
 
 with open(filename, 'rb') as file:
     database = pickle.load(file)
